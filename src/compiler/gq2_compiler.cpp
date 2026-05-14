af13fd5eda5545a0ca9d9e0abcabb3b40cc007a0
Jason Coposky <jason.coposky@gmail.com>
2026-05-14 13:10:01 -0400

    feat(compiler): support advanced operators in AST parsing

--- src/compiler/gq2_compiler.cpp
+++ src/compiler/gq2_compiler.cpp
@@ -1,74 +1,308 @@
 #include "irods/catalog/gq2_compiler.hpp"
 #include "irods/rodsGenQuery.h"
 #include <stdexcept>
+#include <algorithm>
+#include <unordered_set>
+#include <regex>
+#include <iostream>
 
 namespace irods::catalog::compiler {
 
+    static l3kvg::Query::Op parse_operator(std::string_view op_str) {
+        if (op_str == "=") return l3kvg::Query::Op::Eq;
+        if (op_str == "<>" || op_str == "!=") return l3kvg::Query::Op::Ne;
+        if (op_str == ">") return l3kvg::Query::Op::Gt;
+        if (op_str == ">=") return l3kvg::Query::Op::Ge;
+        if (op_str == "<") return l3kvg::Query::Op::Lt;
+        if (op_str == "<=") return l3kvg::Query::Op::Le;
+        if (op_str == "LIKE") return l3kvg::Query::Op::Like;
+        return l3kvg::Query::Op::Eq;
+    }
+
+    struct ParsedCondition {
+        l3kvg::Query::Op op;
+        std::string value;
+    };
+
+    static ParsedCondition parse_condition_string(std::string_view cond) {
+        std::string c(cond);
+        // Regex to match: [operator] 'value'
+        // Example: "= 'foo'", "LIKE '%bar%'", "> 1024"
+        std::regex rx(R"(^\s*(LIKE|NOT LIKE|!=|<>|>=|<=|=|>|<)\s*[']?([^']*)[']?\s*$)");
+        std::smatch match;
+        if (std::regex_search(c, match, rx)) {
+            return { parse_operator(match[1].str()), match[2].str() };
+        }
+        
+        // No operator found, assume exact match on the whole string
+        // Trim optional single quotes if present
+        std::string val = c;
+        if (val.size() >= 2 && val.front() == '\'' && val.back() == '\'') {
+            val = val.substr(1, val.size() - 2);
+        }
+        return { l3kvg::Query::Op::Eq, val };
+    }
+
     const std::unordered_map<int, GraphMap> COLUMN_MAP = {
+        // Data Objects
+        {COL_D_DATA_ID, {"DataObject", "id"}},
+        {COL_D_COLL_ID, {"DataObject", "coll_id"}},
         {COL_DATA_NAME, {"DataObject", "name"}},
+        {COL_DATA_REPL_NUM, {"DataObject", "repl_num"}},
         {COL_DATA_SIZE, {"DataObject", "size"}},
-        {COL_COLL_NAME, {"Collection", "name"}},
-        {COL_USER_NAME, {"User", "name"}},
-        {COL_R_RESC_NAME, {"Resource", "name"}},
+        {COL_D_RESC_NAME, {"DataObject", "resc_name"}},
+        {COL_D_DATA_PATH, {"DataObject", "path"}},
         {COL_D_OWNER_NAME, {"DataObject", "owner"}},
+        {COL_D_OWNER_ZONE, {"DataObject", "owner_zone"}},
+        {COL_D_REPL_STATUS, {"DataObject", "repl_status"}},
+        {COL_D_DATA_CHECKSUM, {"DataObject", "checksum"}},
         {COL_D_CREATE_TIME, {"DataObject", "create_ts"}},
-        {COL_D_MODIFY_TIME, {"DataObject", "modify_ts"}}
+        {COL_D_MODIFY_TIME, {"DataObject", "modify_ts"}},
+        {COL_D_RESC_HIER, {"DataObject", "resc_hier"}},
+        {COL_D_RESC_ID, {"DataObject", "resc_id"}},
+
+        // Collections
+        {COL_COLL_ID, {"Collection", "id"}},
+        {COL_COLL_NAME, {"Collection", "name"}},
+        {COL_COLL_PARENT_NAME, {"Collection", "parent_name"}},
+        {COL_COLL_OWNER_NAME, {"Collection", "owner"}},
+        {COL_COLL_OWNER_ZONE, {"Collection", "owner_zone"}},
+        {COL_COLL_CREATE_TIME, {"Collection", "create_ts"}},
+        {COL_COLL_MODIFY_TIME, {"Collection", "modify_ts"}},
+
+        // Users
+        {COL_USER_ID, {"User", "id"}},
+        {COL_USER_NAME, {"User", "name"}},
+        {COL_USER_TYPE, {"User", "type"}},
+        {COL_USER_ZONE, {"User", "zone"}},
+        {COL_USER_CREATE_TIME, {"User", "create_ts"}},
+        {COL_USER_MODIFY_TIME, {"User", "modify_ts"}},
+
+        // Resources
+        {COL_R_RESC_ID, {"Resource", "id"}},
+        {COL_R_RESC_NAME, {"Resource", "name"}},
+        {COL_R_TYPE_NAME, {"Resource", "type"}},
+        {COL_R_LOC, {"Resource", "location"}},
+        {COL_R_VAULT_PATH, {"Resource", "vault_path"}},
+        {308, {"Resource", "context"}}, // COL_R_RESC_CONTEXT
+        {309, {"Resource", "parent"}},  // COL_R_RESC_PARENT
+        {COL_R_CREATE_TIME, {"Resource", "create_ts"}},
+        {COL_R_MODIFY_TIME, {"Resource", "modify_ts"}},
+
+        // Metadata (AVUs)
+        {COL_META_DATA_ATTR_NAME, {"AVU", "attribute"}},
+        {COL_META_DATA_ATTR_VALUE, {"AVU", "value"}},
+        {COL_META_DATA_ATTR_UNITS, {"AVU", "unit"}},
+        {COL_META_COLL_ATTR_NAME, {"AVU", "attribute"}},
+        {COL_META_COLL_ATTR_VALUE, {"AVU", "value"}},
+        {COL_META_COLL_ATTR_UNITS, {"AVU", "unit"}},
+        {COL_META_DATA_CREATE_TIME, {"AVU", "create_ts"}},
+        {COL_META_DATA_MODIFY_TIME, {"AVU", "modify_ts"}},
+
+        // Zones
+        {COL_ZONE_ID, {"Zone", "id"}},
+        {COL_ZONE_NAME, {"Zone", "name"}},
+        {COL_ZONE_TYPE, {"Zone", "type"}},
+        {COL_ZONE_CONNECTION, {"Zone", "connection"}},
+
+        // Access Control
+        {COL_DATA_ACCESS_NAME, {"Access", "level"}},
+        {COL_DATA_TOKEN_NAMESPACE, {"Access", "namespace"}}
+    };
+
+    const std::unordered_map<std::string_view, GraphMap> COLUMN_NAME_MAP = {
+        {"DATA_ID", {"DataObject", "id"}},
+        {"DATA_NAME", {"DataObject", "name"}},
+        {"DATA_SIZE", {"DataObject", "size"}},
+        {"COLL_ID", {"Collection", "id"}},
+        {"COLL_NAME", {"Collection", "name"}},
+        {"USER_NAME", {"User", "name"}},
+        {"RESC_NAME", {"Resource", "name"}},
+        {"META_DATA_ATTR_NAME", {"AVU", "attribute"}},
+        {"META_DATA_ATTR_VALUE", {"AVU", "value"}},
+        {"META_DATA_ATTR_UNIT", {"AVU", "unit"}},
+        {"META_COLL_ATTR_NAME", {"AVU", "attribute"}},
+        {"META_COLL_ATTR_VALUE", {"AVU", "value"}},
+        {"META_COLL_ATTR_UNIT", {"AVU", "unit"}}
     };
 
     template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
     template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
+    struct condition_visitor : public boost::static_visitor<std::pair<l3kvg::Query::Op, std::string>> {
+        std::pair<l3kvg::Query::Op, std::string> operator()(const irods::experimental::genquery2::condition_equal& c) const { return {l3kvg::Query::Op::Eq, c.string_literal}; }
+        std::pair<l3kvg::Query::Op, std::string> operator()(const irods::experimental::genquery2::condition_not_equal& c) const { return {l3kvg::Query::Op::Ne, c.string_literal}; }
+        std::pair<l3kvg::Query::Op, std::string> operator()(const irods::experimental::genquery2::condition_less_than& c) const { return {l3kvg::Query::Op::Lt, c.string_literal}; }
+        std::pair<l3kvg::Query::Op, std::string> operator()(const irods::experimental::genquery2::condition_less_than_or_equal_to& c) const { return {l3kvg::Query::Op::Le, c.string_literal}; }
+        std::pair<l3kvg::Query::Op, std::string> operator()(const irods::experimental::genquery2::condition_greater_than& c) const { return {l3kvg::Query::Op::Gt, c.string_literal}; }
+        std::pair<l3kvg::Query::Op, std::string> operator()(const irods::experimental::genquery2::condition_greater_than_or_equal_to& c) const { return {l3kvg::Query::Op::Ge, c.string_literal}; }
+        std::pair<l3kvg::Query::Op, std::string> operator()(const irods::experimental::genquery2::condition_like& c) const { return {l3kvg::Query::Op::Like, c.string_literal}; }
+        template<typename T> std::pair<l3kvg::Query::Op, std::string> operator()(const T&) const { return {l3kvg::Query::Op::Eq, ""}; }
+    };
+
     Gq2ToL3kvgCompiler::Gq2ToL3kvgCompiler(l3kvg::Engine& engine) 
         : query_(engine.query()) {}
 
-    l3kvg::Query Gq2ToL3kvgCompiler::compile(const std::vector<AstNode>& ast) {
-        for (const auto& node : ast) {
-            std::visit(overloaded {
-                [this](const ConditionNode& n) { compile_condition(n); },
-                [this](const SelectNode& n)    { compile_select(n); }
-            }, node);
+    l3kvg::Query Gq2ToL3kvgCompiler::compile(const irods::experimental::genquery2::select& ast) {
+        namespace gq = irods::experimental::genquery2;
+        condition_visitor cond_v;
+
+        // 1. Process Projections (Select)
+        struct projection_visitor : public boost::static_visitor<void> {
+            Gq2ToL3kvgCompiler* compiler;
+            projection_visitor(Gq2ToL3kvgCompiler* c) : compiler(c) {}
+            void operator()(const gq::column& col) const {
+                auto it = COLUMN_NAME_MAP.find(col.name);
+                if (it != COLUMN_NAME_MAP.end()) {
+                    compiler->target_node_types_.push_back(it->second.node_type);
+                    compiler->return_fields_.push_back(std::string(it->second.bson_key));
+                }
+            }
+            void operator()(const gq::function& fn) const { /* Handle functions */ }
+        };
+        projection_visitor proj_v(this);
+        for (const auto& proj : ast.projections) {
+            boost::apply_visitor(proj_v, proj);
         }
-        
+
+        // 2. Process Conditions
+        std::vector<gq::condition> conditions;
+        for (const auto& wrap : ast.conditions) {
+            if (auto* cond = boost::get<gq::condition>(&wrap)) {
+                conditions.push_back(*cond);
+            }
+        }
+
+        if (conditions.empty()) {
+            entry_node_type_ = "DataObject";
+            query_.match(entry_node_type_);
+        } else {
+            // Anchor Selection: Prefer AVU if present for optimization
+            const gq::condition* anchor_cond = &conditions.front();
+            for (const auto& cond : conditions) {
+                if (auto* col = std::get_if<gq::column>(&cond.lhs)) {
+                    auto it = COLUMN_NAME_MAP.find(col->name);
+                    if (it != COLUMN_NAME_MAP.end() && it->second.node_type == "AVU") {
+                        anchor_cond = &cond;
+                        break;
+                    }
+                }
+            }
+
+            if (auto* col = std::get_if<gq::column>(&anchor_cond->lhs)) {
+                auto it = COLUMN_NAME_MAP.find(col->name);
+                if (it != COLUMN_NAME_MAP.end()) {
+                    entry_node_type_ = it->second.node_type;
+                    query_.match(entry_node_type_);
+                }
+            }
+
+            // Apply all conditions
+            for (const auto& cond : conditions) {
+                if (auto* col = std::get_if<gq::column>(&cond.lhs)) {
+                    auto it = COLUMN_NAME_MAP.find(col->name);
+                    if (it != COLUMN_NAME_MAP.end()) {
+                        auto [op, val] = boost::apply_visitor(cond_v, cond.expression);
+                        query_.where(it->second.node_type, it->second.bson_key, op, val);
+                    }
+                }
+            }
+        }
+
         resolve_traversals();
         return query_;
     }
 
-    void Gq2ToL3kvgCompiler::compile_condition(const ConditionNode& cond) {
-        auto it = COLUMN_MAP.find(cond.column);
-        if (it == COLUMN_MAP.end()) throw std::runtime_error("Unknown column ID: " + std::to_string(cond.column));
-        
-        auto map = it->second;
-        // Simplified match logic for prototype
-        query_.match(map.node_type).where_eq(map.node_type, map.bson_key, cond.value);
-        entry_node_type_ = map.node_type;
-    }
-
-    void Gq2ToL3kvgCompiler::compile_select(const SelectNode& sel) {
-        for (const auto& col : sel.columns) {
-            auto it = COLUMN_MAP.find(col);
-            if (it == COLUMN_MAP.end()) throw std::runtime_error("Unknown column ID: " + std::to_string(col));
-            
-            auto map = it->second;
-            target_node_types_.push_back(map.node_type);
-            return_fields_.push_back(std::string(map.bson_key));
-        }
-    }
-
     void Gq2ToL3kvgCompiler::resolve_traversals() {
+        std::unordered_set<std::string_view> traversed_aliases;
+        traversed_aliases.insert(entry_node_type_);
+
         for (size_t i = 0; i < target_node_types_.size(); ++i) {
             auto target = target_node_types_[i];
-            if (target != entry_node_type_) {
-                auto edge = find_edge(entry_node_type_, target);
-                query_.out(edge).as(target);
+            if (target == entry_node_type_) {
+                 query_.return_(target, return_fields_[i]);
+                 continue;
+            }
+
+            if (!traversed_aliases.contains(target)) {
+                auto path = find_path(entry_node_type_, target);
+                std::string_view current_source = entry_node_type_;
+                
+                for (const auto& step : path) {
+                    if (!traversed_aliases.contains(step.target_type)) {
+                        if (step.dir == Gq2ToL3kvgCompiler::PathStep::Direction::Out) {
+                            query_.out(step.edge_label).as(step.target_type);
+                        } else {
+                            query_.in(step.edge_label).as(step.target_type);
+                        }
+                        traversed_aliases.insert(step.target_type);
+                    }
+                    current_source = step.target_type;
+                }
             }
             query_.return_(target, return_fields_[i]);
         }
+    }
+
+    std::vector<Gq2ToL3kvgCompiler::PathStep> Gq2ToL3kvgCompiler::find_path(std::string_view source, std::string_view target) {
+        using Dir = Gq2ToL3kvgCompiler::PathStep::Direction;
+
+        // Hardcoded path table for prototype
+        if (source == "Collection" && target == "Resource") {
+            return {{Dir::Out, "CONTAINS", "DataObject"}, {Dir::Out, "REPLICATED_ON", "Resource"}};
+        }
+        if (source == "User" && target == "Resource") {
+            return {{Dir::Out, "HAS_ACCESS", "DataObject"}, {Dir::Out, "REPLICATED_ON", "Resource"}};
+        }
+        if (source == "User" && target == "Collection") {
+            return {{Dir::Out, "HAS_ACCESS", "DataObject"}, {Dir::In, "CONTAINS", "Collection"}};
+        }
+        if (source == "DataObject" && target == "AVU") {
+            return {{Dir::Out, "ANNOTATED_WITH", "AVU"}};
+        }
+        if (source == "AVU" && target == "DataObject") {
+            return {{Dir::In, "ANNOTATED_WITH", "DataObject"}};
+        }
+        if (source == "AVU" && target == "Collection") {
+            return {{Dir::In, "ANNOTATED_WITH", "Collection"}};
+        }
+        
+        if (source == "Zone" && target == "User") {
+            return {{Dir::Out, "HAS_USER", "User"}};
+        }
+        if (source == "Zone" && target == "Collection") {
+            return {{Dir::Out, "HAS_ROOT_COLL", "Collection"}};
+        }
+        if (source == "Zone" && target == "Resource") {
+            return {{Dir::Out, "HAS_RESC", "Resource"}};
+        }
+        
+        if (source == "DataObject" && target == "Replica") {
+            return {{Dir::Out, "HAS_REPLICA", "Replica"}};
+        }
+        if (source == "Replica" && target == "Resource") {
+            return {{Dir::Out, "STAYING_AT", "Resource"}};
+        }
+        if (source == "Replica" && target == "AVU") {
+            return {{Dir::Out, "ANNOTATED_WITH", "AVU"}};
+        }
+        
+        // Fallback to single hop
+        return {{Dir::Out, find_edge(source, target), target}};
     }
 
     std::string_view Gq2ToL3kvgCompiler::find_edge(std::string_view source_type, std::string_view target_type) {
         if (source_type == "User" && target_type == "DataObject") return "HAS_ACCESS";
         if (source_type == "Collection" && target_type == "DataObject") return "CONTAINS";
         if (source_type == "DataObject" && target_type == "Resource") return "REPLICATED_ON";
+        if (source_type == "DataObject" && target_type == "AVU") return "ANNOTATED_WITH";
+        if (source_type == "DataObject" && target_type == "Replica") return "HAS_REPLICA";
+
+        if (target_type == "AVU") return "ANNOTATED_WITH";
+        
+        if (source_type == "Zone" && target_type == "User") return "CONTAINS";
+        if (source_type == "Collection" && target_type == "Collection") return "CONTAINS"; // Parent-Child
+        
         throw std::invalid_argument("No graph path exists between " + std::string(source_type) + " and " + std::string(target_type));
     }
 
